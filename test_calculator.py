d0c1e6899300d5dfd5d0c47e2229d77b326f28f9
Jeff Haineault <jhaineault@Jeffs-MacBook-Pro.local>
2021-10-06 15:48:16 -0700

    Add unit test for calculator

---
+++ test_calculator.py
@@ -0,0 +1,21 @@
+"""
+Unit tests for the calculator library
+"""
+
+
+import calculator
+
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == calculator.add(2, 2)
+        
+    def test_subtraction(self):
+        assert 2 == calculator.subtract(4, 2)
+        
+    def test_multiplication(self):
+        assert 6 == calculator.multiply(2, 3)
+        
+    def test_division(self):
+        assert 1 == calculator.divide(6, 6)
