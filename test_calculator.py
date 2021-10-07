45d2bd755c6f6dbba50509f483de542d2c7179d9
Jeff Haineault <jhaineault@Jeffs-MacBook-Pro.local>
2021-10-07 14:45:00 -0700

    9

--- test_calculator.py
+++ test_calculator.py
@@ -10,12 +10,9 @@ class TestCalculator:
 
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
-        
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
-        
     def test_multiplication(self):
         assert 6 == calculator.multiply(2, 3)
-        
     def test_division(self):
         assert 1 == calculator.divide(6, 6)
